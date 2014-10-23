e1159b3db10d4806919751ce1e5e2e7829d7e619
georgewoskob <georgewoskob@gmail.com>
2014-10-23 17:24:44 -0400

    Can score a spare. Using a frame object.

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -2,6 +2,7 @@ describe("Scores a bowling game", function() {
 	
 	it("Rolling a gutter ball return a score of zero", function() {
 		var bowlingGame = new BowlingGame();
+		bowlingGame.roll(0)
 		bowlingGame.roll(0);
 		expect(bowlingGame.score()).toEqual(0);
 	});
@@ -9,13 +10,32 @@ describe("Scores a bowling game", function() {
 	it("Rolling a two pins down returns a score of two", function() {
 		var bowlingGame = new BowlingGame();
 		bowlingGame.roll(2);
+		bowlingGame.roll(0);
 		expect(bowlingGame.score()).toEqual(2);
 	});
 	
-	it("Rolling 2 pins then three returns 5", function() {
+	it("Rolling 2 pins then 3 returns 5", function() {
 		var bowlingGame = new BowlingGame();
-                bowlingGame.roll(2);
+		bowlingGame.roll(2);
+		bowlingGame.roll(3);
+		expect(bowlingGame.score()).toEqual(5);
+	});
+
+        it("Rolling 4 pins then 3 pins then 2 then 1 returns 10", function() {
+                var bowlingGame = new BowlingGame();
+                bowlingGame.roll(4);
                 bowlingGame.roll(3);
-                expect(bowlingGame.score()).toEqual(5);
+                bowlingGame.roll(2);
+                bowlingGame.roll(1);
+                expect(bowlingGame.score()).toEqual(10);
+        });
+
+	it("Rolling a spare on first frame scores properly", function() {
+		var bowlingGame = new BowlingGame();
+		bowlingGame.roll(5);
+		bowlingGame.roll(5);
+		bowlingGame.roll(3);
+		bowlingGame.roll(0);
+		expect(bowlingGame.score()).toEqual(16);
 	});
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -2,11 +2,36 @@ function BowlingGame() {
 }
 
 BowlingGame.prototype.totalScore = 0;
+function Frame(firstRoll, secondRoll) {
+	this.firstRoll = firstRoll;
+	this.secondRoll = secondRoll;
+};
+BowlingGame.prototype.frames = [];
+BowlingGame.prototype.firstRollInFrame = true;
+BowlingGame.prototype.whichFrameInPlay = 0;
 
 BowlingGame.prototype.roll = function(pinsDown) {
-	this.totalScore += pinsDown;
+	if (this.firstRollInFrame) {
+		this.frames[this.whichFrameInPlay] = new Frame(pinsDown);
+	} else {
+		this.frames[this.whichFrameInPlay].secondRoll = pinsDown;
+		this.whichFrameInPlay++;
+	}
+	this.firstRollInFrame = !this.firstRollInFrame;
 }
 
 BowlingGame.prototype.score = function() {
+	for (var i = 0; i < this.whichFrameInPlay; i++) {
+		var scoreForFrame = 0;
+		
+		scoreForFrame += this.frames[i].firstRoll;
+		scoreForFrame += this.frames[i].secondRoll;
+
+		if (scoreForFrame == 10) {
+			scoreForFrame += this.frames[i + 1].firstRoll;
+		}
+
+		this.totalScore += scoreForFrame;
+	}
 	return this.totalScore;
 }
