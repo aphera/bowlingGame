68e263fc83cb10736a56267372647249340fc735
georgewoskob <georgewoskob@gmail.com>
2014-10-27 14:43:31 -0400

    Made a more intelligent frame object. Playing with different object declaration

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -2,35 +2,48 @@ function BowlingGame() {
 }
 
 BowlingGame.prototype.totalScore = 0;
-function Frame(firstRoll) {
-	this.firstRoll = firstRoll;
-	this.secondRoll = 0;
-};
-
-Frame.prototype.isStrike = function() {
-	return this.firstRoll == 10;
-}
-
-Frame.prototype.isSpare = function() {
-	var tenPinsDown = this.firstRoll + this.secondRoll == 10;
-	console.log(tenPinsDown);
-	return tenPinsDown && !this.isStrike();
-}
-
 BowlingGame.prototype.frames = [];
 BowlingGame.prototype.firstRollInFrame = true;
 BowlingGame.prototype.whichFrameInPlay = 0;
 
+var frame = function (firstRoll) {
+	var firstRoll = firstRoll;
+	var secondRoll = 0;
+
+	return {
+		setSecondRoll: function(roll) {
+			secondRoll = roll;
+		},
+
+		getFirstRoll: function() {
+			return firstRoll;
+		},
+
+		getPinsForFrame: function() {
+			return firstRoll + secondRoll;
+		},
+
+		isStrike: function() {
+			return firstRoll == 10;
+		},
+
+		isSpare: function() {
+			var tenPinsDown = firstRoll + secondRoll == 10;
+			return tenPinsDown && !this.isStrike();
+		}
+	};
+};
+
 BowlingGame.prototype.roll = function(pinsDown) {
 	if (this.firstRollInFrame) {
-		this.frames[this.whichFrameInPlay] = new Frame(pinsDown);
+		this.frames[this.whichFrameInPlay] = frame(pinsDown);
 		if (this.frames[this.whichFrameInPlay].isStrike()) {
 			this.whichFrameInPlay++;
 		} else {
 			this.firstRollInFrame = !this.firstRollInFrame;
 		}
 	} else {
-		this.frames[this.whichFrameInPlay].secondRoll = pinsDown;
+		this.frames[this.whichFrameInPlay].setSecondRoll(pinsDown);
 		this.whichFrameInPlay++;
 		this.firstRollInFrame = !this.firstRollInFrame;
 	}
@@ -41,20 +54,18 @@ BowlingGame.prototype.score = function() {
 		if (i < 10) {
 			var scoreForFrame = 0;
 		
-			scoreForFrame += this.frames[i].firstRoll;
-			scoreForFrame += this.frames[i].secondRoll;
+			scoreForFrame += this.frames[i].getPinsForFrame();
 
 			if (this.frames[i].isSpare()) {
-				scoreForFrame += this.frames[i + 1].firstRoll;
+				scoreForFrame += this.frames[i + 1].getFirstRoll();
 			}
 
 			if (this.frames[i].isStrike()) {
 				if (this.frames[i + 1].isStrike()) {
-					scoreForFrame += this.frames[i + 1].firstRoll;
-					scoreForFrame += this.frames[i + 2].firstRoll;
+					scoreForFrame += this.frames[i + 1].getFirstRoll();
+					scoreForFrame += this.frames[i + 2].getFirstRoll();
 				} else {
-					scoreForFrame += this.frames[i + 1].firstRoll;
-					scoreForFrame += this.frames[i + 1].secondRoll;
+					scoreForFrame += this.frames[i + 1].getPinsForFrame();
 				}
 			}
 
