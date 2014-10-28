ca6c64466b99eef5672889b8593f8d5da2d2ba43
georgewoskob <georgewoskob@gmail.com>
2014-10-28 13:15:39 -0400

    Playing with different object conventions

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,139 +1,139 @@
 describe("A bowling game", function() {
 	
 	it("should give a score of zero for rolling a gutter ball", function() {
-		var bowlingGame = new BowlingGame();
-		bowlingGame.roll(0)
-		bowlingGame.roll(0);
-		expect(bowlingGame.score()).toEqual(0);
+		var thisGame = bowlingGame();
+		thisGame.roll(0)
+		thisGame.roll(0);
+		expect(thisGame.score()).toEqual(0);
 	});
 	
 	it("should give a score of 2 by knocking down 2 pins", function() {
-		var bowlingGame = new BowlingGame();
-		bowlingGame.roll(2);
-		bowlingGame.roll(0);
-		expect(bowlingGame.score()).toEqual(2);
+		var thisGame = bowlingGame();
+		thisGame.roll(2);
+		thisGame.roll(0);
+		expect(thisGame.score()).toEqual(2);
 	});
 	
 	it("should give a score of 5 by knocking down 2 pins then 3", function() {
-		var bowlingGame = new BowlingGame();
-		bowlingGame.roll(2);
-		bowlingGame.roll(3);
-		expect(bowlingGame.score()).toEqual(5);
+		var thisGame = bowlingGame();
+		thisGame.roll(2);
+		thisGame.roll(3);
+		expect(thisGame.score()).toEqual(5);
 	});
 
         it("should give a score of 10 by knocking down 4 pins then 3 pins then 2 then 1", function() {
-                var bowlingGame = new BowlingGame();
-                bowlingGame.roll(4);
-                bowlingGame.roll(3);
-                bowlingGame.roll(2);
-                bowlingGame.roll(1);
-                expect(bowlingGame.score()).toEqual(10);
+                var thisGame = bowlingGame();
+                thisGame.roll(4);
+                thisGame.roll(3);
+                thisGame.roll(2);
+                thisGame.roll(1);
+                expect(thisGame.score()).toEqual(10);
         });
 
 	it("should score a spare on the first frame", function() {
-		var bowlingGame = new BowlingGame();
-		bowlingGame.roll(5);
-		bowlingGame.roll(5);
-		bowlingGame.roll(3);
-		bowlingGame.roll(0);
-		expect(bowlingGame.score()).toEqual(16);
+		var thisGame = bowlingGame();
+		thisGame.roll(5);
+		thisGame.roll(5);
+		thisGame.roll(3);
+		thisGame.roll(0);
+		expect(thisGame.score()).toEqual(16);
 	});
 
         it("should score a strike on first frame", function() {
-                var bowlingGame = new BowlingGame();
-                bowlingGame.roll(10);
-                bowlingGame.roll(3);
-                bowlingGame.roll(6);
-                expect(bowlingGame.score()).toEqual(28);
+                var thisGame = bowlingGame();
+                thisGame.roll(10);
+                thisGame.roll(3);
+                thisGame.roll(6);
+                expect(thisGame.score()).toEqual(28);
         });
 
 	it("should score two strikes in a row properly", function() {
-		var bowlingGame = new BowlingGame();
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(2);
-		bowlingGame.roll(3);
-		expect(bowlingGame.score()).toEqual(42);
+		var thisGame = bowlingGame();
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(2);
+		thisGame.roll(3);
+		expect(thisGame.score()).toEqual(42);
 	});
 
 	it("should score a perfect game", function() {
-		var bowlingGame = new BowlingGame();
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		bowlingGame.roll(10);
-		expect(bowlingGame.score()).toEqual(300);
+		var thisGame = bowlingGame();
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		thisGame.roll(10);
+		expect(thisGame.score()).toEqual(300);
 	});
 
         it("should score a near perfect game", function() {
-                var bowlingGame = new BowlingGame();
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(0);
-                bowlingGame.roll(10);
-                expect(bowlingGame.score()).toEqual(280);
+                var thisGame = bowlingGame();
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(0);
+                thisGame.roll(10);
+                expect(thisGame.score()).toEqual(280);
         });
 
 	it("should score a game that ends in a spare", function() {
-		var bowlingGame = new BowlingGame();
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(10);
-                bowlingGame.roll(5);
-                bowlingGame.roll(5);
-                bowlingGame.roll(4);
-		expect(bowlingGame.score()).toEqual(269);
+		var thisGame = bowlingGame();
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(10);
+                thisGame.roll(5);
+                thisGame.roll(5);
+                thisGame.roll(4);
+		expect(thisGame.score()).toEqual(269);
 	});
 
 
 	it("should know that a strike is 10 pins down on the first roll", function() {
-		var frame = new BowlingGame().frame(10);
-		expect(frame.isStrike()).toEqual(true);
+		var aFrame = bowlingGame().frame(10);
+		expect(aFrame.isStrike()).toEqual(true);
 	});
 
         it("should know that a strike is not 10 pins knocked down over 2 rolls in one frame", function() {
-                var frame = new BowlingGame().frame(9);
-		frame.setSecondRoll(1);
-                expect(frame.isStrike()).toEqual(false);
+                var aFrame = bowlingGame().frame(9);
+		aFrame.setSecondRoll(1);
+                expect(aFrame.isStrike()).toEqual(false);
         });
 
         it("should know that a spare is 10 pins knocked down over 2 rolls in one frame", function() {
-                var frame = new BowlingGame().frame(5);
-		frame.setSecondRoll(5);
-                expect(frame.isSpare()).toEqual(true);
+                var aFrame = bowlingGame().frame(5);
+		aFrame.setSecondRoll(5);
+                expect(aFrame.isSpare()).toEqual(true);
         });
 
         it("should know that if I knock down 10 pins in my first roll of a frame it's not a spare", function() {
-                var frame = new BowlingGame().frame(10);
-                expect(frame.isSpare()).toEqual(false);
+                var aFrame = bowlingGame().frame(10);
+                expect(aFrame.isSpare()).toEqual(false);
         });
 
         it("should know that if I knock down 9 pins over two rolls for a frame it's not a spare", function() {
-                var frame = new BowlingGame().frame(5);
-                frame.setSecondRoll(4);
-                expect(frame.isSpare()).toEqual(false);
+                var aFrame = bowlingGame().frame(5);
+                aFrame.setSecondRoll(4);
+                expect(aFrame.isSpare()).toEqual(false);
         });
 
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,77 +1,78 @@
-function BowlingGame() {
-}
-
-BowlingGame.prototype.totalScore = 0;
-BowlingGame.prototype.frames = [];
-BowlingGame.prototype.firstRollInFrame = true;
-BowlingGame.prototype.whichFrameInPlay = 0;
-
-BowlingGame.prototype.frame = function (firstRoll) {
-	var firstRoll = firstRoll;
-	var secondRoll = 0;
+var bowlingGame = function() {
+	var totalScore = 0;
+	var frames = [];
+	var firstRollInFrame = true;
+	var whichFrameInPlay = 0;
 
 	return {
-		setSecondRoll: function(roll) {
-			secondRoll = roll;
+		frame: function (firstRoll) {
+			var firstRoll = firstRoll;
+			var secondRoll = 0;
+		
+			return {
+				setSecondRoll: function(roll) {
+					secondRoll = roll;
+				},
+		
+				getFirstRoll: function() {
+					return firstRoll;
+				},
+		
+				getPinsForFrame: function() {
+					return firstRoll + secondRoll;
+				},
+		
+				isStrike: function() {
+					return firstRoll == 10;
+				},
+		
+				isSpare: function() {
+					var tenPinsDown = firstRoll + secondRoll == 10;
+					return tenPinsDown && !this.isStrike();
+				}
+			};
 		},
 
-		getFirstRoll: function() {
-			return firstRoll;
+		roll: function(pinsDown) {
+			if (firstRollInFrame) {
+				frames[whichFrameInPlay] = this.frame(pinsDown);
+				if (frames[whichFrameInPlay].isStrike()) {
+					whichFrameInPlay++;
+				} else {
+					firstRollInFrame = !firstRollInFrame;
+				}
+			} else {
+				frames[whichFrameInPlay].setSecondRoll(pinsDown);
+				whichFrameInPlay++;
+				firstRollInFrame = !firstRollInFrame;
+			}
 		},
 
-		getPinsForFrame: function() {
-			return firstRoll + secondRoll;
-		},
-
-		isStrike: function() {
-			return firstRoll == 10;
-		},
-
-		isSpare: function() {
-			var tenPinsDown = firstRoll + secondRoll == 10;
-			return tenPinsDown && !this.isStrike();
+		score: function() {
+			for (var i = 0; i < whichFrameInPlay; i++) {
+				if (i < 10) {
+					var scoreForFrame = 0;
+				
+					scoreForFrame += frames[i].getPinsForFrame();
+		
+					if (frames[i].isSpare()) {
+						scoreForFrame += frames[i + 1].getFirstRoll();
+					}
+		
+					if (frames[i].isStrike()) {
+						if (frames[i + 1].isStrike()) {
+							scoreForFrame += frames[i + 1].getFirstRoll();
+							scoreForFrame += frames[i + 2].getFirstRoll();
+						} else {
+							scoreForFrame += frames[i + 1].getPinsForFrame();
+						}
+					}
+		
+				totalScore += scoreForFrame;
+		
+				}
+			}
+			return totalScore;
 		}
 	};
 };
-
-BowlingGame.prototype.roll = function(pinsDown) {
-	if (this.firstRollInFrame) {
-		this.frames[this.whichFrameInPlay] = this.frame(pinsDown);
-		if (this.frames[this.whichFrameInPlay].isStrike()) {
-			this.whichFrameInPlay++;
-		} else {
-			this.firstRollInFrame = !this.firstRollInFrame;
-		}
-	} else {
-		this.frames[this.whichFrameInPlay].setSecondRoll(pinsDown);
-		this.whichFrameInPlay++;
-		this.firstRollInFrame = !this.firstRollInFrame;
-	}
-}
-
-BowlingGame.prototype.score = function() {
-	for (var i = 0; i < this.whichFrameInPlay; i++) {
-		if (i < 10) {
-			var scoreForFrame = 0;
-		
-			scoreForFrame += this.frames[i].getPinsForFrame();
-
-			if (this.frames[i].isSpare()) {
-				scoreForFrame += this.frames[i + 1].getFirstRoll();
-			}
-
-			if (this.frames[i].isStrike()) {
-				if (this.frames[i + 1].isStrike()) {
-					scoreForFrame += this.frames[i + 1].getFirstRoll();
-					scoreForFrame += this.frames[i + 2].getFirstRoll();
-				} else {
-					scoreForFrame += this.frames[i + 1].getPinsForFrame();
-				}
-			}
-
-		this.totalScore += scoreForFrame;
-
-		}
-	}
-	return this.totalScore;
-}
